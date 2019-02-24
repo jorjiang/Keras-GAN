725b48a236417d0b670c227950f9acf78c12820f
Jiang Ji <jiang.ji@check24.de>
2019-02-24 11:44:06 +0100

    add  create_imgs_from_model() to utils

--- utils/image.py
+++ utils/image.py
@@ -1,10 +1,13 @@
 import base64
 import io
-
+import cv2
 import matplotlib.animation as animation
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import os
+from keras.engine.training import Model
+import numpy as np
+
 def video_from_imgs(img_folder, video_file, fps):
     img_file_list = [f for f in os.listdir(img_folder) if '.png' in f]
     img_files = [os.path.join(img_folder, img_file) for img_file in sorted(img_file_list)]
@@ -27,3 +30,11 @@ def play_video_html(video_file_name):
                     <source src="data:video/mp4;base64,{0}" type="video/mp4" />
                  </video>'''.format(encoded.decode('ascii'))
     return data
+
+def create_imgs_from_model(model: Model, noises: np.ndarray, img_folder: str):
+    for i, noise in enumerate(noises):
+        img = model.predict(noise)
+        file_name = "{}.png".format(str(i).zfill(6))
+        file_path = os.path.join(img_folder, file_name)
+        cv2.imwrite(file_path, img)
+
